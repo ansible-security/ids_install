6f10341c85b0f6fcf5f46789d97afa9073e3839b
Adam Miller <admiller@redhat.com>
2019-01-28 16:24:15 -0600

    add a few more testinfra tests
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,29 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_snort_dir(host):
+    f = host.file('/etc/snort')
+    assert f.is_directory
+
+
+def test_snort_rules_white_list(host):
+    f = host.file('/etc/snort/rules/white_list.rules')
+    assert f.exists
+
+
+def test_snort_rules_black_list(host):
+    f = host.file('/etc/snort/rules/black_list.rules')
+    assert f.exists
+
+
+def test_barnyard_conf(host):
+    f = host.file('/etc/snort/barnyard2.conf')
+    assert f.exists
+
+
+def test_snort_service(host):
+    s = host.service("snort")
+    assert s.is_running
+    assert s.is_enabled
